98d41a9cfbea41a67e0225ce2e6c6dfdaef3f712
Adixdix <adamhabbadi77@gmail.com>
2025-12-02 16:25:35 +0100

    suppression des commentaires de la bdd

--- Code_bdd.sql
+++ Code_bdd.sql
@@ -24,16 +24,10 @@ CREATE TABLE fichier (
 INSERT INTO utilisateur (nom, email, mot_de_passe, admin)
 VALUES ('admin1', 'admin@admin.com', 'f2d81a260dea8a100dd517984e53c56a7523d96942a834bb7fbf2016b4a4583c', 1);
 
--- Script SQL pour creer 2 utilisateurs clients de test
-
 USE projet_c;
 
--- Creer utilisateur 1 (source/expediteur)
 INSERT INTO utilisateur (nom, email, mot_de_passe, admin)
 VALUES ('client1', 'client1@test.com', 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855', 0);
--- Mot de passe hash SHA-256 de "test123"
 
--- Creer utilisateur 2 (destinataire)
 INSERT INTO utilisateur (nom, email, mot_de_passe, admin)
 VALUES ('client2', 'client2@test.com', 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855', 0);
--- Mot de passe hash SHA-256 de "test123"
